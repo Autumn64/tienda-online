a33b253721cac94c05da126e4628e3931ccf4c26
Autumn64 <autumn64@disroot.org>
2025-12-14 15:22:51 -0600

    rc1
    Signed-off-by: Autumn64 <autumn64@disroot.org>

--- src/backend/models/db.sql
+++ src/backend/models/db.sql
@@ -39,34 +39,6 @@ CREATE TABLE IF NOT EXISTS imagenes(
     CONSTRAINT FK_imagenes_producto_id FOREIGN KEY (producto_id) REFERENCES productos(id)
 );
 
-CREATE TABLE IF NOT EXISTS modificaciones(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    producto_id INTEGER NOT NULL,
-    autor_id INTEGER NOT NULL,
-    
-    fecha_modificacion DATETIME,
-    razon VARCHAR(255),
-    
-    CONSTRAINT FK_modificaciones_producto_id FOREIGN KEY (producto_id) REFERENCES productos(id),
-    CONSTRAINT FK_modificaciones_autor_id FOREIGN KEY (autor_id) REFERENCES usuarios(id)
-);
-
--- Categorías
-CREATE TABLE IF NOT EXISTS categorias(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(30),
-    descripcion VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS productos_categorias(
-    producto_id INTEGER NOT NULL,
-    categoria_id INTEGER NOT NULL,
-    
-    PRIMARY KEY (producto_id, categoria_id),
-    CONSTRAINT FK_productos_categorias_producto_id FOREIGN KEY (producto_id) REFERENCES productos(id) ON DELETE CASCADE,
-    CONSTRAINT FK_productos_categorias_categoria_id FOREIGN KEY (categoria_id) REFERENCES categorias(id) ON DELETE CASCADE
-);
-
 -- Transacciones y compras
 CREATE TABLE IF NOT EXISTS compras(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
